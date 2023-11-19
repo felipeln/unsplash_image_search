7027475374674a66b831779432b8dcabd253941c
Felipeln <76855578+felipeln@users.noreply.github.com>
2023-11-19 14:50:25 -0300

    move main to center

--- src/card-image.jsx
+++ src/card-image.jsx
@@ -3,9 +3,9 @@ function CardImage({ id, url, desc, link }) {
 
   return (
     <figure className={id}>
-      <img src={url} alt={desc}/>
-
-      <a className="img-link hide" target="_blank" rel="noreferrer" href={link}></a>
+      <a className="img-link hide" target="_blank" rel="noreferrer" href={link}> 
+        <img src={url} alt={desc} />
+      </a>
     </figure>
   );
 }
